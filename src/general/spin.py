896bd83574be5668fc6b87e7160d7bbce156d8e3
Marcel Koepke <marcel.koepke@gmail.com>
2024-11-27 09:09:20 +0100

    add spinor up and spinor down

--- src/general/spin.py
+++ src/general/spin.py
@@ -1,5 +1,7 @@
 import numpy as np
 
+from .geometry import cart2sphere
+
 pauli_x = np.array([[0, 1], [1, 0]], dtype=np.complex128)
 pauli_y = np.array([[0, -1j], [1j, 0]], dtype=np.complex128)
 pauli_z = np.array([[1, 0], [0, -1]], dtype=np.complex128)
@@ -18,3 +20,23 @@ def spin_operator(axis):
     # project pauli vector
     product = pauli_vector() * axis
     return product.sum(-1)
+
+
+def spinor_up(axis):
+    """Returns the spin up spinor relative to a given axis."""
+    r, theta, phi = cart2sphere(axis)
+
+    component_1 = np.cos(theta / 2)
+    component_2 = np.exp(1j * phi) * np.sin(theta / 2)
+
+    return np.stack([component_1, component_2], axis=-1)
+
+
+def spinor_down(axis):
+    """Returns the spin down spinor relative to a given axis."""
+    r, theta, phi = cart2sphere(axis)
+
+    component_1 = np.sin(theta / 2)
+    component_2 = -np.exp(1j * phi) * np.cos(theta / 2)
+
+    return np.stack([component_1, component_2], axis=-1)
--- src/general/test_spin.py
+++ src/general/test_spin.py
@@ -2,6 +2,7 @@ from unittest import TestCase
 
 from .spin import pauli_x, pauli_y, pauli_z
 from .spin import pauli_vector, spin_operator
+from .spin import spinor_up, spinor_down
 
 import numpy as np
 
@@ -23,3 +24,31 @@ class Test(TestCase):
         so = spin_operator([0, 0, 1])
         self.assertEqual(so.shape, (2, 2))
         self.assertTrue(np.allclose(so, pauli_z))
+
+    def test_spinor_up_down(self):
+        axis = np.array([
+            [1, 0, 0],
+            [0, 1, 0],
+            [0, 0, 1],
+            [1, 1, 1],
+        ])
+
+        up = spinor_up(axis)
+        down = spinor_down(axis)
+
+        up_up = np.einsum("...i,...i", up.conj(), up)
+        down_down = np.einsum("...i,...i", down.conj(), down)
+        up_down = np.einsum("...i,...i", up.conj(), down)
+
+        for i in range(len(axis)):
+            self.assertAlmostEqual(1.0 + 0.0j, up_up[i])
+            self.assertAlmostEqual(1.0 + 0.0j, down_down[i])
+            self.assertAlmostEqual(0.0 + 0.0j, up_down[i])
+
+        for i in range(len(axis)):
+            so = spin_operator(axis[i])
+            so_up = np.einsum("ij,j", so, up[i])
+            so_down = np.einsum("ij,j", so, down[i])
+
+            self.assertTrue(np.allclose(up[i], 1 * so_up))
+            self.assertTrue(np.allclose(down[i], -1 * so_down))
